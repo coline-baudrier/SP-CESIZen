192b93534fd3e731e6f347a51fc4548d92f01bf3
coline_baudrier <coline.baudrier@outlook.com>
2025-03-18 13:42:34 +0100

    Passage sous Ionic

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,43 @@
-import '@/assets/main.css';
-import '@/assets/base.css';
-import './registerSW';
-
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
-import vuetify from './plugins/vuetify';
-
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 import router from './router';
 
-const app = createApp(App);
+import { IonicVue } from '@ionic/vue';
 
-app.use(createPinia());
-app.use(vuetify);
-app.use(router);
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
 
-app.mount('#app');
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* @import '@ionic/vue/css/palettes/dark.always.css'; */
+/* @import '@ionic/vue/css/palettes/dark.class.css'; */
+import '@ionic/vue/css/palettes/dark.system.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router);
+
+router.isReady().then(() => {
+  app.mount('#app');
+});
