d775d182620d6d34c02c8fc55fd7a6545db1680d
coline-baudrier <coline.baudrier@outlook.com>
2025-04-09 22:43:16 +0200

    Ajout des premiers endpoints

---
+++ frontend/services/apiEndpoints.js
@@ -0,0 +1,8 @@
+const API_BASE = "http://51.178.183.31/cesizen/api";
+
+export default {
+  USER: {
+    REGISTER: `${API_BASE}/users/createUser`,
+    LOGIN: `${API_BASE}/users/authUser`,
+  },
+};
